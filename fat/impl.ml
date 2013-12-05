fced5af6517d1c47cc4f4fef4cda86f64ea7342b
David Scott <dave.scott@eu.citrix.com>
2013-12-05 21:05:10 +0000

    The 'fat' commandline tool can now add files to a FAT image

--- fat/impl.ml
+++ fat/impl.ml
@@ -32,6 +32,43 @@ let alloc bytes =
   let pages = Io_page.(to_cstruct (get ((bytes + 4095) / 4096))) in
   Cstruct.sub pages 0 bytes
 
+let with_file flags filename f =
+  Lwt_unix.openfile filename flags 0o0 >>= fun file ->
+  catch (fun () -> f file >>= fun x -> Lwt_unix.close file >>= fun () -> return x) (fun x -> Lwt_unix.close file >>= fun () -> fail x)
+
+let copy_file_in fs outside inside =
+  Lwt_unix.lstat outside >>= fun stats ->
+  let block_size = 1024 * 1024 in
+  let block = alloc block_size in
+  with_file [ Unix.O_RDONLY ] outside
+    (fun ifd ->
+      let rec loop offset remaining =
+        if remaining = 0
+        then return ()
+        else
+          let this = min remaining block_size in
+          let frag = Cstruct.sub block 0 this in
+          Block.really_read ifd frag >>= fun () ->
+          Filesystem.write fs (Filesystem.file_of_path fs inside) offset frag >>= function
+          | `Ok () -> loop (offset + this) (remaining - this)
+          | `Error _ -> failwith "some error" in
+      loop 0 stats.Lwt_unix.st_size
+    )
+
+let run t =
+  try
+    Lwt_main.run t;
+    `Ok ()
+  with Filesystem.Block_device_error `Is_read_only ->
+    `Error(false, "File is read only")
+  | Filesystem.Block_device_error `Unimplemented ->
+    `Error(false, "Block operation is unimplemented")
+  | Filesystem.Block_device_error (`Unknown x) ->
+    `Error(false, x)
+  | Filesystem.Block_device_error _ ->
+    `Error(false, "Unknown block device error")
+
+
 let create common filename size =
   let t =
     ( if Sys.file_exists filename
@@ -53,18 +90,35 @@ let create common filename size =
     Filesystem.make device size >>= fun _ ->
     if common.verb then Printf.printf "Filesystem of size %Ld created\n%!" size;
     return () in
-  try
-    Lwt_main.run t;
-    `Ok ()
-  with Filesystem.Block_device_error `Is_read_only ->
-    `Error(false, "File is read only")
-  | Filesystem.Block_device_error `Unimplemented ->
-    `Error(false, "Block operation is unimplemented")
-  | Filesystem.Block_device_error (`Unknown x) ->
-    `Error(false, x)
-  | Filesystem.Block_device_error _ ->
-    `Error(false, "Unknown block device error")
+  run t
 
 let add common filename files =
+  (* the first entry in [files] is the image name *)
+  let files = List.tl files in
   Printf.fprintf stderr "add %s <- [ %s ]\n%!" filename (String.concat "; " files);
-  `Ok ()
+  let t =
+    Block.connect filename >>|= fun device ->
+    if common.verb then Printf.printf "Opened %s\n%!" filename;
+    Filesystem.openfile device >>= fun fs ->
+    let rec copyin outside_path inside_path file =
+      let outside_path = Filename.concat outside_path file in
+      let inside_path = Path.concat inside_path file in
+      Lwt_unix.stat outside_path >>= fun stats ->
+      match stats.Lwt_unix.st_kind with
+      | Lwt_unix.S_REG ->
+        Printf.fprintf stderr "copyin %s to %s\n%!" outside_path (Path.to_string inside_path);
+        ( Filesystem.create fs inside_path >>= function
+          | `Error _ -> fail (Failure (Printf.sprintf "Failed to create %s" (Path.to_string inside_path)))
+          | `Ok () -> return () ) >>= fun () ->
+        copy_file_in fs outside_path inside_path
+      | Lwt_unix.S_DIR ->
+        let children = Array.to_list (Sys.readdir outside_path) in
+        ( Filesystem.mkdir fs inside_path >>= function
+          | `Error _ -> fail (Failure (Printf.sprintf "Failed to mkdir %s" (Path.to_string inside_path)))
+          | `Ok () -> return () ) >>= fun () ->
+        Lwt_list.iter_s (copyin outside_path inside_path) children
+      | _ ->
+        Printf.fprintf stderr "Skipping file: %s\n%!" outside_path;
+        return () in
+    Lwt_list.iter_s (copyin "" (Path.of_string "/")) files in
+  run t
