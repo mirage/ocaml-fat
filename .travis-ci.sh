88e6f453f0dd1e747b86e305ccb2e56ec82cb454
Anil Madhavapeddy <anil@recoil.org>
2013-12-17 17:42:21 +0000

    Travis: add cmdliner

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 # OPAM packages needed to build tests.
-OPAM_PACKAGES="cstruct ounit lwt cstruct re"
+OPAM_PACKAGES="cstruct ounit lwt cstruct re cmdliner"
 
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
