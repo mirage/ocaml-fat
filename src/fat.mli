35544e6679f9ad705c00d5dca6824700112f1902
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-12-28 12:57:34 +0100

    remove Fat_memoryIO and Fat.MemFS
    Fat_memoryIO has moved in Mirage_block_lwt.Mem (and should probably
    be replaced by ramdisk).

--- src/fat.mli
+++ src/fat.mli
@@ -18,52 +18,22 @@
 
 [@@@ocaml.warning "-34"]
 
-(** FIXME: this should be in io-page *)
-module type IO_PAGE = sig
-
-  val get_buf : ?n:int -> unit -> Cstruct.t
-  (** [get_buf ~n ()] allocates and returns a memory block of [n] pages,
-      represented as a {!Cstruct.t}. If there is not enough memory,
-      an [Out_of_memory] exception is raised. *)
-
-end
-
 (** The main FS interface. *)
-module FS (B: V1_LWT.BLOCK) (M: IO_PAGE): sig
+module FS (B: Mirage_block_lwt.S): sig
 
   type error = [
-    | V1.Fs.error
+    | Mirage_fs.error
     | `Block_read of B.error
   ]
   type write_error = [
     | error
-    | V1.Fs.write_error
+    | Mirage_fs.write_error
     | `Directory_not_empty
     | `Block_write of B.write_error
     | `Exn of exn
   ]
-  include V1_LWT.FS with type error := error and type write_error := write_error
+  include Mirage_fs_lwt.S with type error := error and type write_error := write_error
   val connect : B.t -> t Lwt.t
   val format : B.t -> int64 -> (t, write_error) Result.result Lwt.t
-
-end
-
-(** An in-memory FAT implementation. *)
-module MemFS (M: IO_PAGE): sig
-
-  type error = [
-    | V1.Fs.error
-    | `Block_read of Mirage_block.error
-  ]
-  type write_error = [
-    | error
-    | V1.Fs.write_error
-    | `Directory_not_empty
-    | `Block_write of Mirage_block.write_error
-    | `Exn of exn
-  ]
-  include V1_LWT.FS with type error := error and type write_error := write_error
-  val connect : string -> t Lwt.t
-  val format : string -> int64 -> (t, write_error) Result.result Lwt.t
 
 end
