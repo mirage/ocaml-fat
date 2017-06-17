5e09b5817a8ffeafd65c9787619060fa9daa8f72
David Scott <dave@recoil.org>
2017-06-17 09:57:09 +0100

    Switch to jbuilder
    Signed-off-by: David Scott <dave@recoil.org>

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,14 +1,3 @@
 #!/usr/bin/env ocaml
 #use "topfind"
-#require "topkg"
-open Topkg
-
-let opam =
-  Pkg.opam_file ~lint_deps_excluding:(Some ["ppx_tools"]) "opam"
-
-let () =
-  Pkg.describe ~opams:[opam] "fat-filesystem" @@ fun c ->
-  Ok [ Pkg.mllib ~api:["Fat"] "src/fat.mllib";
-       Pkg.bin   ~dst:"fat" "bin/main";
-       Pkg.test  "test/test";
-       Pkg.test ~run:false "bin/shell"; ]
+#require "topkg-jbuilder.auto"
